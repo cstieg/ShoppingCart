e277e69910261fa1a6453cf2b83a86eb328f04e5
cstieg <cstieg4899@yahoo.com>
2017-10-18 14:41:25 -0400

    Prepare: Shopping cart controller for being copied

--- ShoppingCart/Controllers/ShoppingCartController.cs
+++ ShoppingCart/Controllers/ShoppingCartController.cs
@@ -1,19 +1,25 @@
-﻿using System;
-using System.Data.Entity;
+﻿/*
+using Cstieg.ControllerHelper;
+using Cstieg.ShoppingCart;
+// REPLACE WITH PROJECT NAMESPACE
+using ___________.Models;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
-namespace Cstieg.ShoppingCart
+// REPLACE WITH PROJECT NAMESPACE
+namespace _____________.Controllers
 {
     /// <summary>
     /// Controller to provide shopping cart view
     /// </summary>
     public class ShoppingCartController : Controller
     {
-        //object ClientInfo = new PayPalApiClient().GetClientSecrets();
+        // if using ShoppingCart.PayPal, uncomment this next line and delete the following
+        //ClientInfo ClientInfo = new PayPalApiClient().GetClientSecrets();
         object ClientInfo = new object() { };
 
-        //private DbContext db = new DbContext();
+        ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: ShoppingCart
         public ActionResult Index()
@@ -27,34 +33,137 @@ namespace Cstieg.ShoppingCart
         {
             return View();
         }
-        /*
-        [HttpPost]
-        public ActionResult AddPromoCode()
+        
+        /// <summary>
+        /// Adds a product to the shopping cart
+        /// </summary>
+        /// <param name="id">ID of Product model to add</param>
+        /// <returns>JSON success response if successful, error response if product already exists</returns>
+        [HttpPost, ActionName("AddOrderDetailToShoppingCart")]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddOrderDetailToShoppingCart(int id)
         {
+            // look up product entity
+            Product product = db.Products.SingleOrDefault(m => m.Id == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Retrieve shopping cart from session
             ShoppingCart shoppingCart = ShoppingCart.GetFromSession(HttpContext);
+
+            // Add new order detail to session
             try
             {
-                string pc = Request.Params.Get("PromoCode");
-                PromoCode promoCode = db.PromoCodes.Where(p => p.Code.ToLower() == pc.ToLower()).Single();
-
-                shoppingCart.AddPromoCode(promoCode);
-
+                shoppingCart.AddProduct(product);
                 shoppingCart.SaveToSession(HttpContext);
-                return Redirect("Index");
-            }
-            catch (InvalidOperationException e)
-            {
-                ModelState.AddModelError("PromoCodes", "Failed to add promocode: Invalid promo code");
-                ViewBag.ClientInfo = ClientInfo;
-                return View("Index", shoppingCart);
+                return this.JOk();
             }
             catch (Exception e)
             {
-                ModelState.AddModelError("PromoCodes", "Failed to add promocode: " + e.Message);
-                ViewBag.ClientInfo = ClientInfo;
-                return View("Index", shoppingCart);
+                return this.JError(403, e.Message);
             }
         }
-        */
+
+        /// <summary>
+        /// Increases the quantity of a product in the shopping cart
+        /// </summary>
+        /// <param name="id">ID of Product model to add</param>
+        /// <returns>JSON success response</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult IncrementItemInShoppingCart(int id)
+        {
+            // look up product entity
+            Product product = db.Products.SingleOrDefault(m => m.Id == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Retrieve shopping cart from session
+            ShoppingCart shoppingCart = ShoppingCart.GetFromSession(HttpContext);
+
+            // Increment quantity and save shopping cart
+            try
+            {
+                shoppingCart.IncrementProduct(product);
+                shoppingCart.SaveToSession(HttpContext);
+                return this.JOk();
+            }
+            catch (Exception e)
+            {
+                return this.JError(403, e.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Decreases the quantity of an item in the shopping cart
+        /// </summary>
+        /// <param name="id">ID of the Product model qty to decrement</param>
+        /// <returns>JSON success response</returns>
+        [HttpPost, ActionName("DecrementItemInShoppingCart")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DecrementItemInShoppingCart(int id)
+        {
+            // look up product entity
+            Product product = db.Products.SingleOrDefault(m => m.Id == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Retrieve shopping cart from session
+            ShoppingCart shoppingCart = ShoppingCart.GetFromSession(HttpContext);
+
+            // Decrement qty and update shopping cart in session
+            try
+            {
+                shoppingCart.DecrementProduct(product);
+                shoppingCart.SaveToSession(HttpContext);
+                return this.JOk();
+            }
+            catch (Exception e)
+            {
+                return this.JError(403, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Removes a Product from the shopping cart
+        /// </summary>
+        /// <param name="id">ID of Product model to remove</param>
+        /// <returns>JSON success response</returns>
+        [HttpPost, ActionName("RemoveItemInShoppingCart")]
+        [ValidateAntiForgeryToken]
+        public ActionResult RemoveItemInShoppingCart(int id)
+        {
+            // look up product entity
+            Product product = db.Products.SingleOrDefault(m => m.Id == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Retrieve shopping cart from session
+            ShoppingCart shoppingCart = ShoppingCart.GetFromSession(HttpContext);
+
+            // Remove Product and update shopping cart in session
+            try
+            {
+                shoppingCart.RemoveProduct(product);
+                shoppingCart.SaveToSession(HttpContext);
+                return this.JOk();
+            }
+            catch (Exception e)
+            {
+                return this.JError(403, e.Message);
+            }
+        }
+       
+
     }
 }
+*/
---
+++ ShoppingCart/Readme.md
@@ -0,0 +1,14 @@
+﻿
+
+
+Steps to integrate
+
+In solution, add existing project ShoppingCart
+In main project, add reference to ShoppingCart.dll
+
+Inherit ProductBase class in Product class in main project Models
+
+Add references to Customer, Order, OrderDetails, ShipToAddress (Cstieg.ShoppingCart) in ApplicationDbContext
+Add reference to Product model in ApplicationDbContext
+
+Copy ShoppingCartController to controller in main project, substitute namespaces where necessary
