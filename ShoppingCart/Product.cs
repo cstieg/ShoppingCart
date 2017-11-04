153022310f6bac41424a5b782001e12f67f575c8
cstieg <cstieg4899@yahoo.com>
2017-11-04 16:56:31 -0400

    Add: WebImages model, updates to Product model

--- ShoppingCart/Product.cs
+++ ShoppingCart/Product.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
 
 namespace Cstieg.ShoppingCart
 {
@@ -15,17 +17,18 @@ namespace Cstieg.ShoppingCart
         [StringLength(50, MinimumLength = 2)]
         public virtual string Name { get; set; }
 
-        [StringLength(100)]
-        public virtual string Description { get; set; }
-
         public virtual decimal Price { get; set; }
 
         public virtual decimal Shipping { get; set; }
 
-        [DisplayName("Product Image")]
-        public virtual string ImageUrl { get; set; }
-        public virtual string ImageSrcSet { get; set; }
-        
+        [Display(Name = "Product Images")]
+        [InverseProperty("Product")]
+        public virtual ICollection<WebImage> WebImages { get; set; }
+
+        [StringLength(2000)]
+        [AllowHtml]
+        public string ProductInfo { get; set; }
+
         [Display(Name = "Featured")]
         public virtual bool DisplayOnFrontPage { get; set; }
 
---
+++ ShoppingCart/WebImage.cs
@@ -0,0 +1,14 @@
+﻿using Cstieg.Image;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cstieg.ShoppingCart
+{
+    public class WebImage : WebImageBase
+    {
+        [ForeignKey("Product")]
+        public int? ProductId { get; set; }
+        [JsonIgnore]
+        public virtual ProductBase Product { get; set; }
+    }
+}
