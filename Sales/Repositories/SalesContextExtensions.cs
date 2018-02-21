e44f9531ad53f50de7e84c6489c86338a13bf9d0
cstieg <cstieg4899@yahoo.com>
2018-02-21 12:11:53 -0500

    Add: Constraints to upsert

--- Sales/Repositories/SalesContextExtensions.cs
+++ Sales/Repositories/SalesContextExtensions.cs
@@ -1,25 +1,39 @@
 ﻿using Cstieg.Sales.Interfaces;
+using System;
 using System.Data.Entity;
 
 namespace Cstieg.Sales.Repositories
 {
     public static class SalesContextExtensions
     {
-        public static void Upsert(this ISalesDbContext context, ISalesEntity entity)
+        public static void Upsert<TEntity>(this ISalesDbContext context, TEntity entity) where TEntity : class, ISalesEntity
         {
-            if (IsSavedToDatabase(context, entity))
+            if (IsSavedToDatabase(entity))
             {
                 context.Entry(entity).State = EntityState.Modified;
             }
             else
             {
-                context.Entry(entity).State = EntityState.Added;
+                IDbSet<TEntity> dbSet = GetDbSet<TEntity>(context, entity);
+                dbSet.Add(entity);
             }
         }
 
-        public static bool IsSavedToDatabase(this ISalesDbContext context, ISalesEntity entity)
+        public static bool IsSavedToDatabase(ISalesEntity entity)
         {
             return entity.Id != 0;
         }
+
+        public static IDbSet<TEntity> GetDbSet<TEntity>(ISalesDbContext context, ISalesEntity entity) where TEntity : class
+        {
+            foreach (var property in context.GetType().GetProperties())
+            {
+                if (property.PropertyType.GenericTypeArguments[0] == entity.GetType())
+                    return (IDbSet<TEntity>)property.GetValue(context);
+            }
+            throw new Exception("DbSet not found");
+        }
+
+        
     }
 }
