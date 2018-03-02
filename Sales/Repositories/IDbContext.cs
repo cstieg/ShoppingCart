b42673cf000a77468936c807de1702ad4e7f558e
cstieg <cstieg4899@yahoo.com>
2018-03-02 12:22:44 -0500

    Refactor: Separate DbContext stuff into IDbContext interface

---
+++ Sales/Repositories/IDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
+
+namespace Cstieg.Sales.Repositories
+{
+    public interface IDbContext
+    {
+        DbSet Set(Type entityType);
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        DbEntityEntry Entry(object entity);
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+        Task<int> SaveChangesAsync();
+    }
+}
--- Sales/Repositories/ISalesDbContext.cs
+++ Sales/Repositories/ISalesDbContext.cs
@@ -9,7 +9,7 @@ namespace Cstieg.Sales.Repositories
     /// <summary>
     /// Interface for database context that contains the tables needed for the shopping cart system
     /// </summary>
-    public interface ISalesDbContext
+    public interface ISalesDbContext : IDbContext
     {
         DbSet<Store> Stores { get; set; }
         DbSet<ShoppingCart> ShoppingCarts { get; set; }
@@ -24,12 +24,6 @@ namespace Cstieg.Sales.Repositories
         DbSet<ShippingCountry> ShippingCountries { get; set; }
         DbSet<ShippingScheme> ShippingSchemes { get; set; }
         DbSet<WebImage> WebImages { get; set; }
-
-        DbSet Set(Type entityType);
-        DbSet<TEntity> Set<TEntity>() where TEntity : class;
-        DbEntityEntry Entry(object entity);
-        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
-        Task<int> SaveChangesAsync();
     }
 
 }
